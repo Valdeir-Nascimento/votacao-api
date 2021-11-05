85eb9aede967b01ae6666f26f6cc0cee98d15246
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-05 18:42:44 -0300

    Criação do repositório de Sessão

---
+++ src/main/java/br/com/votacao/api/repository/SessaoRepository.java
@@ -0,0 +1,13 @@
+package br.com.votacao.api.repository;
+
+import br.com.votacao.api.model.Sessao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessaoRepository extends JpaRepository<Sessao, Long> {
+
+    Optional<List<Sessao>> findByPautaId(Long id);
+
+}
