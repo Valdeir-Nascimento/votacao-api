7f4577432449c3d8004861ac8eae924984875bf4
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-05 23:05:49 -0300

    Ajuste na migration do flyway e criação de script para popular as tabelas.

--- src/main/resources/db/migration/V003__cria-tabela-sessao.sql
+++ src/main/resources/db/migration/V003__cria-tabela-sessao.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS `votacao_api`.`sessao` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `data_inicio` DATETIME NOT NULL,
   `minutos` INT NOT NULL,
-  `paula_id` INT NOT NULL,
+  `pauta_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_sessao_paula_idx` (`paula_id` ASC) VISIBLE,
+  INDEX `fk_sessao_paula_idx` (`pauta_id` ASC) VISIBLE,
   CONSTRAINT `fk_sessao_paula`
-      FOREIGN KEY (`paula_id`)
+      FOREIGN KEY (`pauta_id`)
           REFERENCES `votacao_api`.`pauta` (`id`)
           ON DELETE NO ACTION
           ON UPDATE NO ACTION)
