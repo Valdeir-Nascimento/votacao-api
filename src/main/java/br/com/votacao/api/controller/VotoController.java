e6dec2b28fc756a5f16107bb762a197358ce3759
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-08 20:05:09 -0300

    Criação serviço Rest POST para salvar voto.

--- src/main/java/br/com/votacao/api/controller/VotoController.java
+++ src/main/java/br/com/votacao/api/controller/VotoController.java
@@ -2,12 +2,17 @@ package br.com.votacao.api.controller;
 
 import br.com.votacao.api.dto.VotoDTO;
 import br.com.votacao.api.dto.converter.VotoDTOConverter;
+import br.com.votacao.api.event.RecursoCriadoEvent;
 import br.com.votacao.api.model.Voto;
 import br.com.votacao.api.service.VotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,6 +23,8 @@ public class VotoController {
     private VotoService votoService;
     @Autowired
     private VotoDTOConverter votoDTOConverter;
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping("/sessoes/votos")
     public ResponseEntity<List<VotoDTO>> listar() {
@@ -29,6 +36,17 @@ public class VotoController {
     public ResponseEntity<VotoDTO> findById(@PathVariable Long idVoto) {
         Voto voto = votoService.buscar(idVoto);
         return ResponseEntity.ok().body(votoDTOConverter.to(voto));
+    }
+
+    @PostMapping("/{idPauta}/sessoes/votos")
+    public ResponseEntity<VotoDTO> criarVoto(
+            @PathVariable Long idPauta,
+            @PathVariable Long idSessao,
+            @Valid @RequestBody VotoDTO votoDTO, HttpServletResponse response) {
+        Voto voto = votoDTOConverter.to(votoDTO);
+        voto = votoService.salvarVoto(idPauta, idSessao, voto);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, voto.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(votoDTOConverter.to(voto));
     }
 
     @DeleteMapping("/sessoes/votos/{idVoto}")
