b0f33b56c8a8da77811c0422e3dc5da15c5f9627
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-08 10:51:29 -0300

    Criação Rest POST para criar sessão.

--- src/main/java/br/com/votacao/api/controller/SessaoController.java
+++ src/main/java/br/com/votacao/api/controller/SessaoController.java
@@ -2,13 +2,16 @@ package br.com.votacao.api.controller;
 
 import br.com.votacao.api.dto.SessaoDTO;
 import br.com.votacao.api.dto.converter.SessaoDTOConverter;
+import br.com.votacao.api.event.RecursoCriadoEvent;
 import br.com.votacao.api.model.Sessao;
 import br.com.votacao.api.service.SessaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -19,6 +22,8 @@ public class SessaoController {
     private SessaoService sessaoService;
     @Autowired
     private SessaoDTOConverter sessaoDTOConverter;
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @GetMapping("/sessoes")
     public ResponseEntity<List<SessaoDTO>> findAll() {
@@ -32,10 +37,20 @@ public class SessaoController {
         return ResponseEntity.ok().body(sessaoDTOConverter.to(sessao));
     }
 
+    @GetMapping("/{idPauta}/sessoes")
+    public ResponseEntity<SessaoDTO> criarSessao(@PathVariable Long idPauta, @RequestBody SessaoDTO sessaoDTO, HttpServletResponse response) {
+        Sessao sessao = sessaoDTOConverter.to(sessaoDTO);
+        sessao = sessaoService.criarSessao(idPauta, sessao);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, sessao.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(sessaoDTOConverter.to(sessao));
+    }
+
     @DeleteMapping("/sessoes/{idSessao}")
     @ResponseStatus(HttpStatus.OK)
     public void excluir(@PathVariable Long idSessao) {
         sessaoService.excluir(idSessao);
     }
+
+
 
 }
