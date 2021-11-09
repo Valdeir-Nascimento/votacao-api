a14816cc3e8fae617969609c35d1da97b12bde55
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-09 19:48:45 -0300

    Melhorando o tratamento de exceptions ao excluir uma sessão.

--- src/main/java/br/com/votacao/api/service/SessaoService.java
+++ src/main/java/br/com/votacao/api/service/SessaoService.java
@@ -1,10 +1,15 @@
 package br.com.votacao.api.service;
 
+import br.com.votacao.api.exception.EntidadeEmUsoException;
+import br.com.votacao.api.exception.PautaNaoEncontradaException;
 import br.com.votacao.api.exception.SessaoNaoEncontradaException;
 import br.com.votacao.api.model.Pauta;
 import br.com.votacao.api.model.Sessao;
 import br.com.votacao.api.repository.SessaoRepository;
+import br.com.votacao.api.util.MessagesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,13 +45,20 @@ public class SessaoService {
     }
 
     public void excluir(Long idSessao) {
-        sessaoRepository.deleteById(idSessao);
+        try {
+            sessaoRepository.deleteById(idSessao);
+            sessaoRepository.flush();
+        } catch (EmptyResultDataAccessException e) {
+            throw new SessaoNaoEncontradaException(idSessao);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format(MessagesUtil.MSG_ENTIDADE_EM_USO, idSessao));
+        }
     }
 
     public Sessao buscarSessaoPorPauta(Long idSessao, Long idPauta) {
         return sessaoRepository
                 .findByIdAndPautaId(idSessao, idPauta)
-                .orElseThrow(() -> new SessaoNaoEncontradaException("Sessão não encontrada"));
+                .orElseThrow(() -> new SessaoNaoEncontradaException("Sessão e/ou Pauta não encontrada"));
     }
 
 }
