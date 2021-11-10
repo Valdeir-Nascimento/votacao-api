ba750eb3cc1b96c183e9343fedc3607e686b8b58
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-09 22:56:57 -0300

    Criação de metodo para exibir resultado da votação por pauta.

---
+++ src/main/java/br/com/votacao/api/controller/ResultadoVotacaoController.java
@@ -0,0 +1,25 @@
+package br.com.votacao.api.controller;
+
+import br.com.votacao.api.dto.VotacaoDTO;
+import br.com.votacao.api.service.ResultadoVotacaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "Resultado Votacao")
+public class ResultadoVotacaoController {
+
+    @Autowired
+    private ResultadoVotacaoService resultadoVotacaoService;
+
+    @ApiOperation(value = "Exibe Resultado da Votação por Pauta")
+    @GetMapping
+    public VotacaoDTO buscarResultadoVotacaoPorPauta(@PathVariable Long id) {
+        return resultadoVotacaoService.obterResultadoVotacao(id);
+    }
+
+}
--- src/main/java/br/com/votacao/api/service/ResultadoVotacaoService.java
+++ src/main/java/br/com/votacao/api/service/ResultadoVotacaoService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class TopicoVotacaoService {
+public class ResultadoVotacaoService {
 
     @Autowired
     private VotoRepository votoRepository;
